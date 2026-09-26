233bc710fb8c20fd5e5d04c0d7ac1f75f8478b38
Chavda Dharmesh <dharmesh.chavda@keyannatech.com>
2026-09-26 20:41:13 +0530

    changes of

--- controller/categorycontroller.js
+++ controller/categorycontroller.js
@@ -1,16 +1,25 @@
-const Category = require('../models/category')
-const mongoose = require('mongoose')
+const Category = require("../models/category");
+const mongoose = require("mongoose");
 
-const create = async(req,res)=>{
-    try{
-        let body = req.body
-         body = {...body,createdBy:req.userId}
-        await Category.create(body)
-        res.status(200).send('Category created')
-    }
-    catch(error){
-        res.status(400).send(error)
-    }
-}
 
-module.exports = {create}
+const create = async (req, res) => {
+  console.log("lklkl")
+  const session = await mongoose.startSession();
+  try {
+    await session.startTransaction();
+    let body = req.body;
+    body = { ...body, createdBy: req.userId }; 
+    await Category.create([body],{session});
+    console.log('kkkk')
+    await Category.findByIdAndUpdate(['6a7df21723bd0bbc50214b8c',{$set:{name:'dd'}}],{session})
+    await session.commitTransaction();
+    res.status(200).send("Category created");
+  } catch (error) {
+    res.status(400).send(error);
+    await session.abortTransaction();
+  } finally {
+    session.endSession();
+  }
+};
+
+module.exports = { create };
